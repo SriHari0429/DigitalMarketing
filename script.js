dc604fddd617197dba93d318f2d7d3e51daaedac
SriHari0429 <126370604+SriHari0429@users.noreply.github.com>
2023-11-03 14:14:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const nodemailer = require('nodemailer');
+function scrollto(section){
+    let scroll=document.getElementById(section)
+    scroll.scrollIntoView({ behavior: 'smooth' });
+}
+
+
+
+
+
+
+
+
+
